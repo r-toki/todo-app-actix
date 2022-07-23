63ca62adbeb7133c7f3b90831fa65ad3eead57df
rtokkie <natto.and.eggs@gmail.com>
2022-07-23 18:48:37 +0900

    rename

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-mod controllers;
+mod controller;
 
 use actix_web::{middleware::Logger, App, HttpServer};
 use std::env;
@@ -7,14 +7,14 @@ use std::env;
 async fn main() -> std::io::Result<()> {
     env_logger::init_from_env(env_logger::Env::default().default_filter_or("info"));
 
-    // NOTE: set
+    // NOTE: $ heroku config:set HOST=0.0.0.0
     let host = env::var("HOST").unwrap_or_else(|_| "127.0.0.1".to_string());
     let port = env::var("PORT").unwrap_or_else(|_| "8080".to_string());
 
     HttpServer::new(move || {
         App::new()
             .wrap(Logger::default())
-            .service(controllers::tasks::index)
+            .service(controller::tasks::index)
     })
     .bind(format!("{}:{}", host, port))?
     .run()
